5a65ced1f7c7493ea7ddad87dbb56ce4b45ca0b3
Stef Heyenrath <stef.heyenrath@gmail.com>
2020-04-02 14:47:23 +0000

    .

--- ConsoleAppBUnit/Program.cs
+++ ConsoleAppBUnit/Program.cs
@@ -3,14 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
+using System.Reflection.Emit;
 using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using Bunit;
+using Castle.DynamicProxy;
+using ChangeTracking;
+using FakeItEasy;
 using FizzWare.NBuilder;
 using Microsoft.AspNetCore.Components;
-using Moq;
 using NSubstitute;
 using NSubstitute.ReceivedExtensions;
+using IInvocation = Moq.IInvocation;
 
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
 
@@ -258,18 +262,270 @@ namespace ConsoleAppBUnit
                 return variances;
             }
 
+            private static Expression<Func<T>> CreatePropertyGetterExpression<T>(PropertyInfo property)
+            {
+                Type entityType = typeof(T);
+                var parameter = Expression.Parameter(entityType, "e");
+                Type funcType = typeof(Func<>).MakeGenericType(typeof(T));
+                //var body = Expression.MakeMemberAccess(parameter, property);
+                return (Expression<Func<T>>)Expression.Lambda(funcType, parameter);
+            }
+
+            public static Expression<Func<T>> Get<T>(PropertyInfo pInfo)
+            {
+                ParameterExpression parameter = Expression.Parameter(typeof(T));
+                MemberExpression property = Expression.Property(parameter, pInfo);
+                Type funcType = typeof(Func<>).MakeGenericType(typeof(T));
+
+                //next line fails: can't convert int to object
+
+                LambdaExpression lambda;
+                if (typeof(T).IsClass == false && typeof(T).IsInterface == false)
+                    lambda = Expression.Lambda(funcType, Expression.Convert(property, typeof(Object)), parameter);
+                else
+                    lambda = Expression.Lambda(funcType, property, parameter);
+
+                return (Expression<Func<T>>)lambda;
+            }
+
+            //private static TypeBuilder CreateImplementationType(string name, PropertyInfo[] properties)
+            //{
+            //    TypeBuilder typeBuilder = moduleBuilder.DefineType(name,
+            //        TypeAttributes.Public |
+            //        TypeAttributes.Class |
+            //        TypeAttributes.AutoClass |
+            //        TypeAttributes.AnsiClass |
+            //        TypeAttributes.AutoLayout,
+            //        typeof(object));
+
+            //    foreach (PropertyInfo pi in properties)
+            //    {
+            //        string piName = pi.Name;
+            //        Type propertyType = pi.PropertyType;
+
+            //        FieldBuilder field = typeBuilder.DefineField("_" + piName, propertyType, FieldAttributes.Private);
+
+            //        MethodInfo getMethod = pi.GetGetMethod();
+            //        if (getMethod != null)
+            //        {
+            //            MethodBuilder methodBuilder = typeBuilder.DefineMethod(
+            //                getMethod.Name,
+            //                MethodAttributes.Public |
+            //                MethodAttributes.SpecialName |
+            //                MethodAttributes.Virtual,
+            //                propertyType,
+            //                Type.EmptyTypes);
+
+            //            var ilGenerator = methodBuilder.GetILGenerator();
+            //            ilGenerator.Emit(OpCodes.Ldarg_0); // Load "this"
+            //            ilGenerator.Emit(OpCodes.Ldfld, field); // Load the backing field
+            //            ilGenerator.Emit(OpCodes.Ret); // Return the value on the stack (field)
+
+            //            // Override the get method from the original type to this IL-generated version
+            //            typeBuilder.DefineMethodOverride(methodBuilder, getMethod);
+            //        }
+
+            //        var setMethod = pi.GetSetMethod();
+            //        if (setMethod != null)
+            //        {
+            //            var methodBuilder = typeBuilder.DefineMethod(
+            //                setMethod.Name,
+            //                MethodAttributes.Public |
+            //                MethodAttributes.SpecialName |
+            //                MethodAttributes.Virtual,
+            //                typeof(void),
+            //                new Type[] { pi.PropertyType });
+
+            //            var ilGenerator = methodBuilder.GetILGenerator();
+            //            ilGenerator.Emit(OpCodes.Ldarg_0); // Load "this"
+            //            ilGenerator.Emit(OpCodes.Ldarg_1); // Load the set value
+            //            ilGenerator.Emit(OpCodes.Stfld, field); // Set the field to the set value
+            //            ilGenerator.Emit(OpCodes.Ret);
+
+            //            // Override the set method from the original type to this IL-generated version
+            //            typeBuilder.DefineMethodOverride(methodBuilder, setMethod);
+            //        }
+            //    }
+
+            //    return typeBuilder;
+            //}
+
+            public class Interceptor : IInterceptor
+            {
+                public void Intercept(IInvocation invocation)
+                {
+
+                }
+
+                public void Intercept(Castle.DynamicProxy.IInvocation invocation)
+                {
+                    Console.WriteLine($"Before target call {invocation.Method.Name}");
+                    try
+                    {
+                        invocation.Proceed();
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine($"Target exception {e.Message}");
+                        throw;
+                    }
+                    finally
+                    {
+                        Console.WriteLine($"After target call {invocation.Method.Name}");
+                    }
+                }
+            }
+
+
+            public class MyClassBuilder
+            {
+                AssemblyName asemblyName;
+                public MyClassBuilder(string className)
+                {
+                    asemblyName = new AssemblyName(className);
+                }
+                public object CreateObject(string[] propertyNames, Type[] types)
+                {
+                    if (propertyNames.Length != types.Length)
+                    {
+                        Console.WriteLine("The number of property names should match their corresopnding types number");
+                    }
+
+                    TypeBuilder dynamicClass = CreateClass();
+                    CreateConstructor(dynamicClass);
+                    for (int ind = 0; ind < propertyNames.Count(); ind++)
+                        CreateProperty(dynamicClass, propertyNames[ind], types[ind]);
+                    Type type = dynamicClass.CreateType();
+
+                    return Activator.CreateInstance(type);
+                }
+                private TypeBuilder CreateClass()
+                {
+                    AssemblyBuilder assemblyBuilder = AssemblyBuilder.DefineDynamicAssembly(asemblyName, AssemblyBuilderAccess.Run);
+                    ModuleBuilder moduleBuilder = assemblyBuilder.DefineDynamicModule("MainModule");
+                    TypeBuilder typeBuilder = moduleBuilder.DefineType(asemblyName.FullName
+                                        , TypeAttributes.Public |
+                                        TypeAttributes.Class |
+                                        TypeAttributes.AutoClass |
+                                        TypeAttributes.AnsiClass |
+                                        TypeAttributes.BeforeFieldInit |
+                                        TypeAttributes.AutoLayout
+                                        , null);
+                    return typeBuilder;
+                }
+                private void CreateConstructor(TypeBuilder typeBuilder)
+                {
+                    typeBuilder.DefineDefaultConstructor(MethodAttributes.Public | MethodAttributes.SpecialName | MethodAttributes.RTSpecialName);
+                }
+                private void CreateProperty(TypeBuilder typeBuilder, string propertyName, Type propertyType)
+                {
+                    FieldBuilder fieldBuilder = typeBuilder.DefineField("_" + propertyName, propertyType, FieldAttributes.Private);
+
+                    PropertyBuilder propertyBuilder = typeBuilder.DefineProperty(propertyName, PropertyAttributes.HasDefault, propertyType, null);
+                    MethodBuilder getPropMthdBldr = typeBuilder.DefineMethod("get_" + propertyName, MethodAttributes.Public | MethodAttributes.SpecialName | MethodAttributes.HideBySig | MethodAttributes.Virtual, propertyType, Type.EmptyTypes);
+                    ILGenerator getIl = getPropMthdBldr.GetILGenerator();
+
+                    getIl.Emit(OpCodes.Ldarg_0);
+                    getIl.Emit(OpCodes.Ldfld, fieldBuilder);
+                    getIl.Emit(OpCodes.Ret);
+
+                    MethodBuilder setPropMthdBldr = typeBuilder.DefineMethod("set_" + propertyName,
+                          MethodAttributes.Public |
+                          MethodAttributes.SpecialName |
+                          MethodAttributes.HideBySig,
+                          null, new[] { propertyType });
+
+                    ILGenerator setIl = setPropMthdBldr.GetILGenerator();
+                    Label modifyProperty = setIl.DefineLabel();
+                    Label exitSet = setIl.DefineLabel();
+
+                    setIl.MarkLabel(modifyProperty);
+                    setIl.Emit(OpCodes.Ldarg_0);
+                    setIl.Emit(OpCodes.Ldarg_1);
+                    setIl.Emit(OpCodes.Stfld, fieldBuilder);
+
+                    setIl.Emit(OpCodes.Nop);
+                    setIl.MarkLabel(exitSet);
+                    setIl.Emit(OpCodes.Ret);
+
+                    propertyBuilder.SetGetMethod(getPropMthdBldr);
+                    propertyBuilder.SetSetMethod(setPropMthdBldr);
+                }
+            }
+
+
+            public Action<object> Convert<T>(Action<T> myActionT)
+            {
+                if (myActionT == null)
+                {
+                    return null;
+                }
+
+                return o => myActionT((T)o);
+            }
+
             public IRenderedComponent<TComponent> RenderComponent6<TComponent>(params Action<TComponent>[] actions) where TComponent : class, IComponent, new()
             {
                 var componentParameters = new List<ComponentParameter>();
-
+                //var ccc = new Com();
                 foreach (var action in actions)
                 {
+                    var props = typeof(TComponent).GetProperties();
+                    foreach (var p in props)
+                    {
+                        //var ex = CreatePropertyGetterExpression(typeof(TComponent), p);
+                        var ex2 = CreatePropertyGetterExpression<TComponent>(p);
+                        // A.CallToSet(() => fc2.Name)).i
+                    }
+                    var mc = (object)new MyClassBuilder(typeof(TComponent).Name + "Dynamic").CreateObject(props.Select(p => p.Name).ToArray(), props.Select(p => p.PropertyType).ToArray());
+
+                    var ast = new TComponent();
+                    //var ast2 = ast.AsTrackable();
+
+                    //var trackable = ast2.CastToIChangeTrackable();
+
+                    var cp = (TComponent)new ProxyGenerator().CreateClassProxy(typeof(TComponent), new Type[0],
+                        new ProxyGenerationOptions
+                        {
+
+                        });
+                    //var ct = new ProxyGenerator().CreateInterfaceProxyWithTarget()
+
+                    //var cp  = order.AsTrackable();
+
+
+                    //var fc2 = A.Fake<Com>();
+                    var fc = A.Fake<TComponent>();
+
+                    // Use the conventional .NET prefix "set_" to refer to a property's setter:
+                    A.CallTo(fc).Where(call => call.Method.Name != "set_Address")
+                        .Throws(new Exception("we can't move"));
+
+                    A.CallTo(fc).Where(call => call != null).Invokes(x =>
+                    {
+                        int xxxx = 9;
+                    });
+                    // .Throws(new Exception("we can't move"));
+
+
+
+
+
                     var orig = new TComponent();
                     var n = new TComponent();
 
+                    //var action2 = Convert(action);
+
+                    //action2.Invoke(mc);
                     action.Invoke(n);
 
-                    var name = action.Method.Name;
+                    //cp.Execute();
+
+                    var calls = Fake.GetCalls(fc).ToList();
+                    var man = Fake.GetFakeManager(fc);
+
+
+                    //var name = action.Method.Name;
                     var com = DetailedCompare(orig, n);
 
                     var variance = com.FirstOrDefault();
