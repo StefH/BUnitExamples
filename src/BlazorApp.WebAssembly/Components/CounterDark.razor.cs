c3b68090a248317b08cfbb28022d7a54ca6ec206
Stef Heyenrath <stef.heyenrath@gmail.com>
2020-06-29 20:15:01 +0200

    wrapper

---
+++ src/BlazorApp.WebAssembly/Components/CounterDark.razor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorApp.WebAssembly.Components
+{
+    public partial class CounterDark
+    {
+        [CascadingParameter]
+        public bool IsDark { get; set; }
+
+        [Parameter]
+        public int Start { get; set; } = 0;
+
+        private void Minus()
+        {
+            Start--;
+        }
+    }
+}
---
+++ src/BlazorApp.WebAssembly/Components/Wrapper.razor.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+
+namespace BlazorApp.WebAssembly.Components
+{
+    public partial class Wrapper : ComponentBase
+    {
+        [Parameter]
+        public bool IsDark { get; set; } = true;
+
+
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+        protected override void BuildRenderTree(RenderTreeBuilder builder) => builder.AddContent(0, ChildContent);
+    }
+}
