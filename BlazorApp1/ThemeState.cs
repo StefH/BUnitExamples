61e49e9457262396e73a03903d989c79b4cb797f
Stef Heyenrath <stef.heyenrath@gmail.com>
2020-06-29 18:59:35 +0200

    projects

--- BlazorApp1/ThemeState.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace BlazorApp1
-{
-    public class ThemeState
-    {
-        public bool IsDarkMode { get; set;  }
-    }
-}
---
+++ test/BlazorApp.WebAssembly.Tests/UnitTest1.cs
@@ -0,0 +1,22 @@
+using Bunit;
+using Xunit;
+
+namespace BlazorApp.WebAssembly.Tests
+{
+    public class UnitTest1 : TestContext
+    {
+        [Fact]
+        public void CounterShouldIncrementWhenClicked()
+        {
+            // Arrange: render the Counter.razor component
+            var cut = RenderComponent<Counter>();
+
+            // Act: find and click the <button> element to increment
+            // the counter in the <p> element
+            cut.Find("button").Click();
+
+            // Assert: first find the <p> element, then verify its content
+            cut.Find("p").MarkupMatches("<p>Current count: 1</p>");
+        }
+    }
+}
