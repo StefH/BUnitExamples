34a83d398e178881436fe9dd631f760a7ee6116a
Stef Heyenrath <stef.heyenrath@gmail.com>
2020-07-07 17:29:31 +0200

    7

---
+++ test/BlazorApp.WebAssembly.Tests/4. CounterDarkTests.cs
@@ -0,0 +1,36 @@
+﻿using BlazorApp.WebAssembly.Components;
+using Bunit;
+using FluentAssertions;
+using Xunit;
+
+namespace BlazorApp.WebAssembly.Tests
+{
+    public class CounterDarkTests : TestContext
+    {
+        [Fact]
+        public void CounterDark_Should_Use_UnnamedCascadingParameter_IsDark_True()
+        {
+            // Arrange / Act: render the CounterDark.razor component
+            var counterComponent = RenderComponent<CounterDark>(componentParameterBuilder =>
+            {
+                componentParameterBuilder.Add(true);
+            });
+
+            // Assert: first find the <div> element, then verify its style
+            counterComponent.Find("div").Attributes.GetNamedItem("style").Value.Should().Contain("darkgrey");
+        }
+
+        [Fact]
+        public void CounterDark_Should_Use_UnnamedCascadingParameter_IsDark_False()
+        {
+            // Arrange / Act: render the CounterDark.razor component
+            var counterComponent = RenderComponent<CounterDark>(componentParameterBuilder =>
+            {
+                componentParameterBuilder.Add(false);
+            });
+
+            // Assert: first find the <div> element, then verify its style
+            counterComponent.Find("div").Attributes.GetNamedItem("style").Value.Should().Contain("white");
+        }
+    }
+}
