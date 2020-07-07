f229e98264973cc05d04cab91e91d6f1f94ad54e
Stef Heyenrath <stef.heyenrath@gmail.com>
2020-07-07 12:21:51 +0000

    verifying the final state of a Component

--- test/BlazorApp.WebAssembly.Tests/CounterTest.cs
+++ test/BlazorApp.WebAssembly.Tests/CounterTest.cs
@@ -1,5 +1,6 @@
 using BlazorApp.WebAssembly.Pages;
 using Bunit;
+using FluentAssertions;
 using Xunit;
 
 namespace BlazorApp.WebAssembly.Tests
@@ -14,12 +15,18 @@ namespace BlazorApp.WebAssembly.Tests
             // Arrange: render the Counter.razor component
             var counterComponent = ctx.RenderComponent<Counter>();
 
+            // Assert: verifying the initial state of a Component
+            counterComponent.Instance.CurrentCount.Should().Be(0);
+
             // Act: find and click the <button> element to increment
             // the counter in the <p> element
             counterComponent.Find("button").Click();
 
             // Assert: first find the <p> element, then verify its content
             counterComponent.Find("p").MarkupMatches("<p>Current count: 1</p>");
+
+            // Assert: verifying the final state of a Component
+            counterComponent.Instance.CurrentCount.Should().Be(1);
         }
     }
 }
